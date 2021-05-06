c8a3cc08dca61a6641b2bfbf21c21dab3b1dcad5
Thi Doãn <t@thi.im>
2021-05-06 22:58:14 +0900

    Strip swift tarball

--- tools/strip-clang.sh
+++ tools/strip-clang.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 
 curl -LO https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
 tar -xf clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
-pushd llvmorg-12.0.0/clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-20.04
+pushd clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-20.04
 mkdir bin-new
 mv \
   bin/clang \
---
+++ tools/strip-swift.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Strip swift tarball to include only what we need to speed up the
+# decompression at toolchain resolution.
+
+set -euo pipefail
+
+curl -LO https://swift.org/builds/swift-5.3.3-release/ubuntu2004/swift-5.3.3-RELEASE/swift-5.3.3-RELEASE-ubuntu20.04.tar.gz
+tar -xf swift-5.3.3-RELEASE-ubuntu20.04.tar.gz
+pushd swift-5.3.3-RELEASE-ubuntu20.04/usr
+mkdir bin-new
+mv \
+  bin/swift \
+  bin/swiftc \
+  bin-new
+rm -rf bin lib libexec include share
+mv bin-new bin
+popd
+tar -Jcf swift-5.3.3-RELEASE-ubuntu20.04.tar.xz swift-5.3.3-RELEASE-ubuntu20.04
